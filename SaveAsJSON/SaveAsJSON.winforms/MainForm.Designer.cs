e3a88049d5af4bb70c4c065674e5e8423961e663
Wout Ongena <wout.ongena@student.odisee.be>
2021-08-07 17:19:22 +0200

    Added Empty Projects

---
+++ SaveAsJSON/SaveAsJSON.winforms/MainForm.Designer.cs
@@ -0,0 +1,83 @@
+﻿
+namespace SaveAsJSON.winforms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Namelabel = new System.Windows.Forms.Label();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Namelabel
+            // 
+            this.Namelabel.AutoSize = true;
+            this.Namelabel.Location = new System.Drawing.Point(12, 9);
+            this.Namelabel.Name = "Namelabel";
+            this.Namelabel.Size = new System.Drawing.Size(49, 17);
+            this.Namelabel.TabIndex = 0;
+            this.Namelabel.Text = "Name:";
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Location = new System.Drawing.Point(12, 29);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(180, 22);
+            this.NameTextBox.TabIndex = 1;
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(12, 57);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(180, 23);
+            this.SaveButton.TabIndex = 2;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(210, 102);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.NameTextBox);
+            this.Controls.Add(this.Namelabel);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Namelabel;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.Button SaveButton;
+    }
+}
